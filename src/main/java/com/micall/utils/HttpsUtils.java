3f3eb12ede55599d275d4f87eece3d7812b44eb6
unknown <870070285@qq.com>
2022-01-25 14:45:01 +0800

    HttpUtils最后封装

--- src/main/java/com/micall/utils/HttpsUtils.java
+++ src/main/java/com/micall/utils/HttpsUtils.java
@@ -1,16 +1,12 @@
 package com.micall.utils;
 
+import com.alibaba.fastjson.JSONObject;
 import com.micall.constant.Constants;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpRequest;
-import org.apache.http.NoHttpResponseException;
-import org.apache.http.client.ClientProtocolException;
+import org.apache.http.*;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.*;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -24,14 +20,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLHandshakeException;
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.UnknownHostException;
 import java.security.NoSuchAlgorithmException;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class HttpsUtils {
@@ -40,24 +40,6 @@ public class HttpsUtils {
     private static final String ENCODING = "UTF-8";
     private static final String RESULT = "-1";
 
-    //    //设置请求和传输超时时间
-//    private static RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(10000).setSocketTimeout(10000).build();
-//    private static CloseableHttpClient httpClient = HttpClients.createDefault();
-//    public static String send(String httpUrl,String message) throws IOException {
-//        String result = null;
-//        HttpPost httpPost = new HttpPost(httpUrl);
-//        // 设置数据读取超时5s,传输超时5s,连接请求超时5s
-//        RequestConfig requestConfig = RequestConfig.custom()
-//                .setSocketTimeout(5000)
-//                .setConnectTimeout(50000)
-//                .setConnectionRequestTimeout(5000).build();
-//        httpPost.setConfig(requestConfig);
-//        message = URLEncoder.encode(message,"utf-8");
-//        StringEntity entity = new StringEntity(message);
-//        httpPost.setEntity(entity);
-//        CloseableHttpResponse response = httpClient.execute(httpPost);
-//        BufferedReader in = null;
-//}
     /**
         初始化连接池管理器，配置SSL
      */
@@ -273,18 +255,33 @@ public class HttpsUtils {
      * @param isAuthentication 是否需要鉴权
      * @return
      */
-    public static String httpPostForm(String url,String params,String language,boolean isAuthentication){
+    public static String httpPostForm(String url, Map<String,Object> params, String language, boolean isAuthentication){
         // 定义post请求对象并传入url
         HttpPost post = new HttpPost(url);
         // 传入请求头
-        post.setHeader(Constants.REQ_HEADER_TYPE_NAME,Constants.REQ_HEADER_TYPE_VALUE_FROM);
-        post.setHeader(Constants.REQ_HEADER_LANGUAGE_Name,language);
+        post.setHeader("Content-type","application/x-www-form-urlencoded");
         // 添加鉴权头
         if(isAuthentication){
             AuthorizationUtils.setTokenInRequest(post);
         }
         // 传入请求参数
-        post.setEntity(new StringEntity(params,"UTF-8"));
+        if(null != params && params.size() >0){
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            // 通过map继承entitySet方法获取entity
+            Set<Map.Entry<String,Object>> entitySet =params.entrySet();
+            // 循环遍历，获取迭代器
+            Iterator<Map.Entry<String,Object>> iterator = entitySet.iterator();
+            while (iterator.hasNext()){
+                Map.Entry<String,Object> mapEntry = iterator.next();
+                nameValuePairs.add(new BasicNameValuePair(mapEntry.getKey(),mapEntry.getValue().toString()));
+            }
+            // 为post设置封装好的请求参数
+            try{
+                post.setEntity(new UrlEncodedFormEntity(nameValuePairs,"UTF-8"));
+            }catch (UnsupportedEncodingException e){
+                logger.info("请求参数设置失败"+e);
+            }
+        }
         // 定义响应体
         CloseableHttpResponse response =null;
         try {
@@ -310,7 +307,206 @@ public class HttpsUtils {
         }
         return null;
     }
-
+    /**
+     * json格式的put请求
+     * @param url 请求地址
+     * @param params 请求参数
+     * @param isAuthentication 是否需要鉴权
+     * @return
+     */
+    public static String httpPutJson(String url,String params,String language,boolean isAuthentication){
+        // 定义put请求对象并传入url
+        HttpPut put = new HttpPut(url);
+        // 传入请求头
+        put.setHeader(Constants.REQ_HEADER_TYPE_NAME,Constants.REQ_HEADER_TYPE_VALUE_JSON);
+        put.setHeader(Constants.REQ_HEADER_LANGUAGE_Name,language);
+        // 添加鉴权头
+        if(isAuthentication){
+            AuthorizationUtils.setTokenInRequest(put);
+        }
+        // 传入请求参数
+        put.setEntity(new StringEntity(params,"UTF-8"));
+        // 定义响应体
+        CloseableHttpResponse response =null;
+        try {
+            // 获取客户端连接
+            CloseableHttpClient client =getHttpClient();
+            // 发起请求
+            response=client.execute(put);
+            // 获取响应体
+            HttpEntity entity = response.getEntity();
+            // 响应体内容转为字符串类型
+            String reqBody = EntityUtils.toString(entity);
+            return reqBody;
+        }catch (Exception e){
+            logger.info("put请求异常"+e);
+        }finally {
+            if(response !=null){
+                try {
+                    response.close(); // 关闭连接
+                }catch (IOException e2){
+                    logger.info("关闭连接出错"+e2);
+                }
+            }
+        }
+        return null;
+    }
+    /**
+     * Form格式的put请求
+     * @param url 请求地址
+     * @param params 请求参数
+     * @param isAuthentication 是否需要鉴权
+     * @return
+     */
+    public static String httpPutForm(String url, Map<String,Object> params, String language, boolean isAuthentication){
+        // 定义put请求对象并传入url
+        HttpPut put = new HttpPut(url);
+        // 传入请求头
+        put.setHeader("Content-type","application/x-www-form-urlencoded");
+        // 添加鉴权头
+        if(isAuthentication){
+            AuthorizationUtils.setTokenInRequest(put);
+        }
+        // 传入请求参数
+        if(null != params && params.size() >0){
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            // 通过map继承entitySet方法获取entity
+            Set<Map.Entry<String,Object>> entitySet =params.entrySet();
+            // 循环遍历，获取迭代器
+            Iterator<Map.Entry<String,Object>> iterator = entitySet.iterator();
+            while (iterator.hasNext()){
+                Map.Entry<String,Object> mapEntry = iterator.next();
+                nameValuePairs.add(new BasicNameValuePair(mapEntry.getKey(),mapEntry.getValue().toString()));
+            }
+            // 为post设置封装好的请求参数
+            try{
+                put.setEntity(new UrlEncodedFormEntity(nameValuePairs,"UTF-8"));
+            }catch (UnsupportedEncodingException e){
+                logger.info("请求参数设置失败"+e);
+            }
+        }
+        // 定义响应体
+        CloseableHttpResponse response =null;
+        try {
+            // 获取客户端连接
+            CloseableHttpClient client =getHttpClient();
+            // 发起请求
+            response=client.execute(put);
+            // 获取响应体
+            HttpEntity entity = response.getEntity();
+            // 响应体内容转为字符串类型
+            String reqBody = EntityUtils.toString(entity);
+            return reqBody;
+        }catch (Exception e){
+            logger.info("Post请求异常"+e);
+        }finally {
+            if(response !=null){
+                try {
+                    response.close(); // 关闭连接
+                }catch (IOException e2){
+                    logger.info("关闭连接出错"+e2);
+                }
+            }
+        }
+        return null;
+    }
+    public static class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase{
+        // 定义一个delete请求方法
+        private static final String REQ_METHOD_NAME = "DELETE";
+        @Override
+        public String getMethod() {
+            return REQ_METHOD_NAME;
+        }
+        public HttpDeleteWithBody(final String uri){
+            super();
+            setURI(URI.create(uri));
+        }
+        public HttpDeleteWithBody(final URI uri){
+            super();
+            setURI(uri);
+        }
+        public HttpDeleteWithBody(){
+            super();
+        }
+    }
+    public static String httpDeleteJson(String url,String param,String language,boolean isAuthentication){
+        // 创建delete对象并传入url
+        HttpDeleteWithBody delete = new HttpDeleteWithBody(url);
+        // 传入请求头
+        delete.setHeader(Constants.REQ_HEADER_TYPE_NAME,Constants.REQ_HEADER_TYPE_VALUE_JSON);
+        delete.setHeader(Constants.REQ_HEADER_LANGUAGE_Name,language);
+        // 添加鉴权头
+        if(isAuthentication){
+            AuthorizationUtils.setTokenInRequest(delete);
+        }
+        // 传入请求参数
+        delete.setEntity(new StringEntity(param,"UTF-8"));
+        // 定义响应体
+        CloseableHttpResponse response = null;
+        try{
+            CloseableHttpClient client = getHttpClient();
+            response = client.execute(delete);
+            HttpEntity entity = response.getEntity();
+            String reqBody = EntityUtils.toString(entity);
+            return reqBody;
+        }catch (Exception e){
+            logger.info("delete请求异常" +e);
+        }finally {
+            if(response !=null){
+                try {
+                    response.close();
+                }catch (IOException e2){
+                    logger.info("关闭连接出错"+e2);
+                }
+            }
+        }
+        return null;
+    }
+    public static String call(String url,String params,String reqMethod,String submitType,String language,boolean isAuthentication){
+        try {
+            // 如果是json的提交方式
+            if("json".equalsIgnoreCase(submitType)){
+                // 判断是get请求方式时
+                if("get".equalsIgnoreCase(reqMethod)){
+                    // 返回get请求的响应体
+                    return httpGetJson(url,language,isAuthentication);
+                }else if("post".equalsIgnoreCase(reqMethod)){
+                    return httpPostJson(url,params,language,isAuthentication);
+                }else if ("put".equalsIgnoreCase(reqMethod)){
+                    return httpPutJson(url,params,language,isAuthentication);
+                }else if("delete".equalsIgnoreCase(reqMethod)){
+                    return httpDeleteJson(url,params,language,isAuthentication);
+                }
+            }else if("form".equalsIgnoreCase(submitType)){
+                if("get".equalsIgnoreCase(reqMethod)){
+                    params = jsonToKeyValue(params);
+                    return httpGetForm(url,params,language,isAuthentication);
+                }else if("post".equalsIgnoreCase(reqMethod)){
+                    return httpGetForm(url,params,language,isAuthentication);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String jsonToKeyValue(String jsonParams){
+        // json转换成map
+        Map<String,String> map = JSONObject.parseObject(jsonParams,HashMap.class);
+        // 把获取到的键值保存到set集合中
+        Set<String> keySet =map.keySet();
+        // 定义一个参数结果
+        String params = "";
+        for (String key : keySet){
+            // 通过KEY获取到value值
+            String value = map.get(key);
+            // 最后的请求参数
+            params += key + "=" +value +"&";
+        }
+        // 得到最终的参数值后，把最后一个“&”去掉
+        params = params.substring(0,params.length()-1);
+        return params;
+    }
 }
 
 
